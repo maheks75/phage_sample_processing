c2099dcdf0dcc746fb602c81788c374f2bee47a8
ANTIBODY-PROCESSING <137297069+maheks75@users.noreply.github.com>
2024-12-24 16:03:22 +0000

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,49 +1,16 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "bef43a52-15b4-4df9-bd40-c58a019d0003",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from Bio import SeqIO\n",
-    "\n",
-    "seq_count = {}\n",
-    "\n",
-    "with open('sorted_sheep_heavylight.txt', 'w') as file:\n",
-    "    for i, record in enumerate(SeqIO.parse('mereged_partial.assembled.fastq', 'fastq')):\n",
-    "        seq_data = str(record.seq)\n",
-    "        print(seq_data)    \n",
-    "        if seq_data in seq_count:\n",
-    "            seq_count[seq_data] += 1\n",
-    "        else:\n",
-    "            seq_count[seq_data] = 1\n",
-    "    sorted_seq_count = sorted(seq_count.items(), key=lambda x: x[1], reverse=True)\n",
-    "    for key, value in sorted_seq_count:\n",
-    "        file.write(f'{key}:{value}\\n')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from Bio import SeqIO
+
+seq_count = {}
+
+with open('sorted_sheep_heavylight.txt', 'w') as file:
+    for i, record in enumerate(SeqIO.parse('mereged_partial.assembled.fastq', 'fastq')):
+        seq_data = str(record.seq)
+        print(seq_data)
+        if seq_data in seq_count:
+            seq_count[seq_data] += 1
+        else:
+            seq_count[seq_data] = 1
+    sorted_seq_count = sorted(seq_count.items(), key=lambda x: x[1], reverse=True)
+    for key, value in sorted_seq_count:
+        file.write(f'{key}:{value}\n')
+
